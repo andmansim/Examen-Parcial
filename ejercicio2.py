e7abfeb7da6dded4fc7a544cd997c04eb69fcac8
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-14 11:26:56 +0100

    juagador 2 y guardando en el fichero

--- ejercicio2.py
+++ ejercicio2.py
@@ -35,7 +35,7 @@ for x in range (0,3):
         else:
             f.write(str(board[x][y] + '\t'))
 f.close()
-
+# Player 1
 print("Player 1 are the red rooks and player 2 the green ones.")
 print("Player 1: choose the rook that you want to move. By saying the line and column where it is, remember that the lines and columns go from 0 to 2")
 print("Line of the piece:")
@@ -60,4 +60,43 @@ elif player1_column == 2:
     board[player1_line][y] = " "
 for x in board:
     print(" ".join(x))
-        
+
+# fichero
+f = open('HackerChess.txt', 'a')
+f.write("Player1" + '\n')
+for x in range (0,3):
+    for y in range(0,3):
+        if y == 2:
+            f.write(str(board[x][y] + '\n'))
+        else:
+            f.write(str(board[x][y] + '\t'))
+f.close()
+    
+#Jugador 2
+print("Player 2: choose the rook that you want to move. By saying the line and column where it is, remember that the lines and columns go from 0 to 2")
+print("Line of the piece:")
+player2_line = int(input())
+print("Column of the piece:")
+player2_column = int(input())
+print("New line of the piece:")
+player2_new_line = int(input())
+
+
+if player2_column == 0:
+    y = 0
+    board[player2_new_line][y] = board[player2_line][y]
+    board[player2_line][y] = " "
+elif player1_column == 1:
+    y = 1
+    board[player2_new_line][y] = board[player2_line][y]
+    board[player2_line][y] = " "
+elif player1_column == 2:
+    y = 2
+    board[player2_new_line][y] = board[player2_line][y]
+    board[player2_line][y] = " "
+for x in board:
+    print(" ".join(x))
+
+while board[player1_new_line][y] != board[player2_new_line][y]:
+    
+    
