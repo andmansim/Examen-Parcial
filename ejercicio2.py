18d93c13b95d2ec6897205700be74d62991640cb
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-14 10:49:31 +0100

    crear tablero

--- ejercicio2.py
+++ ejercicio2.py
@@ -7,3 +7,20 @@ board = [[R,G , G],
 green_rook = ((0,1), (0,2), (2,0))
 red_rook = ((0,0), (1,1), (1,2))
 
+def boardcreation():
+    line = []
+    for i in range(0,3):
+        for j in range (0,3):
+            if tuple([i,j]) in green_rook:
+               line.append("G") 
+            elif tuple([i,j]) in red_rook:
+                line.append("R")
+            else:
+                line.append(" ")
+        board.append(line)
+        line = []
+        
+boardcreation()
+
+for x in board:
+    print("  ".join(x))
