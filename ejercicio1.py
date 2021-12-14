2f1c3e4e01eb87d264a6aabbee228e5d8c0cf2c4
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-14 12:03:27 +0100

    jugador 1 vocales

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,5 +4,14 @@ initial_word = list('BANANA')
 print(initial_word)
 print("Player 1 have to make words started by vowels")
 print("Player 2 have to make words started by consonant")
+print("Player 1:")
 player_1 = str(input())
-player_2 
+print("Player 2:")
+player_2 = str(input())
+player_1_list = list(player_1)
+player_2_list = list(player_2)
+
+vowels = ['A']
+consonant = ['B', 'N']
+if player_1_list[0] == vowels[0]:
+    
