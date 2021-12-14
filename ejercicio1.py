4cae06a311f516b406105131faeeb30022e85b47
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-14 11:55:53 +0100

    jugadores

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+# BANANA
+print("The word is 'BANANA'")
+initial_word = list('BANANA')
+print(initial_word)
+print("Player 1 have to make words started by vowels")
+print("Player 2 have to make words started by consonant")
+player_1 = str(input())
+player_2 
